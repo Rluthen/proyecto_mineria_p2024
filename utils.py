c509d96f26b99aac8479238c2a0cde0afb8128b6
yo-lo-pregunto <junjo.mendoza11@gmail.com>
2024-05-06 00:02:04 -0600

    Clean up of 2_eliminacion_criterio.ipynb

--- utils.py
+++ utils.py
@@ -4,13 +4,10 @@ import os
 OUT_DIR = 'out_datasets'
 IN_DIR = 'in_datasets'
 
-FILENAMES = ['1_base_limpia.csv']
 DATASET = './in_datasets/Base_PreguntasFormulario-1erDebatePresidencial2024.xlsx'
 
-def save_df(df: pd.DataFrame, idx: int, **kwargs) -> None:
-    if idx >= len(FILENAMES):
-        raise IndexError
+def save_df(df: pd.DataFrame, name: str, **kwargs) -> None:
     if not os.path.exists(OUT_DIR):
         os.mkdir(OUT_DIR)
-    filename = os.path.join(OUT_DIR, FILENAMES[idx])
+    filename = os.path.join(OUT_DIR, name)
     df.to_csv(filename, **kwargs)
